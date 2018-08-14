523c13e8bcad3a00b47b4b94878f2e1e4ac8b379
Hidde de Vries <hidde@hiddedevries.nl>
2018-08-14 14:37:12 +0200

    Pass userId, add error component
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

---
+++ src/components/Error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+};
+</script>
+
+<style>
+  .error {
+    border: 1px solid red;
+    padding: 1em;
+    margin-bottom: 1em;
+  }
+    .error p {
+      margin: 0;
+    }
+    .error p + p {
+      margin-top: 1em;
+    }
+</style>
--- src/views/PageProfile.vue
+++ src/views/PageProfile.vue
@@ -1,8 +1,12 @@
 <template>
-  <ApolloQuery :query="profileQuery">
+  <ApolloQuery :query="profileQuery" :variables="{ userId }">
     <template slot-scope="{ result: { loading, data, error } }">
       <template v-if="loading">Loading...</template>
-      <template v-else-if="error">An error occured</template>
+      <template v-else-if="error">
+        <Error><h2>{{ error.message }}</h2>
+          <pre>{{ error }}</pre>
+          <p>An error occured while trying to go to {{ userId }}: </p></Error>
+        </template>
       <template v-else-if="data">
         <Profile v-bind="data.profiles[0]"></Profile>
       </template>
@@ -13,15 +17,18 @@
 
 <script>
 import Profile from '@/components/Profile.vue';
+import Error from '@/components/Error.vue';
 import { PROFILE_QUERY } from '@/queries/profile';
 
 export default {
   name: 'PageProfile',
   components: {
     Profile,
+    Error,
   },
   data() {
     return {
+      userId: this.$route.params.userId,
       profileQuery: PROFILE_QUERY,
     };
   },
