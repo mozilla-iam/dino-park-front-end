21319b70ec160243bfa7eac4e684ceb7b25a0428
Hidde de Vries <hidde@hiddedevries.nl>
2018-08-14 14:36:49 +0200

    List profiles on homepage
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,5 +1,36 @@
+
 <template>
-  <main>
-    <h1>Mozillians front-end</h1>
-  </main>
+  <ApolloQuery :query="profileList">
+    <template slot-scope="{ result: { loading, data, error } }">
+      <template v-if="loading">Loading...</template>
+      <template v-else-if="error">An error occured</template>
+      <template v-else-if="data">
+        <main>
+          <PersonList v-if="data.profiles">
+            <Person v-for="profile in data.profiles" v-bind="profile"></Person>
+          </PersonList>
+        </main>
+      </template>
+      <template v-else>No result :(</template>
+    </template>
+  </ApolloQuery>
 </template>
+
+<script>
+import Person from '@/components/Person';
+import PersonList from '@/components/PersonList';
+import { PROFILE_LIST } from '@/queries/profile';
+
+export default {
+  name: 'PageHome',
+  components: {
+    Person,
+    PersonList
+  },
+  data() {
+    return {
+      profileList: PROFILE_LIST,
+    };
+  },
+};
+</script>
