1259b03c2812fb4e3d53c2475f6e7854558b42e4
Hidde de Vries <hidde@hiddedevries.nl>
2018-08-14 19:16:25 +0200

    Remove link to profile, make it just home
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/profile">Profile</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <router-view/>
   </div>
