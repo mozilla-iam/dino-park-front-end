26a81ae1ad5acd4898772adfe428db7d764cd045
Hidde de Vries <hidde@hiddedevries.nl>
2018-09-12 09:08:16 +0200

    Move ProfileNav to its own component
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

---
+++ src/components/ProfileNav.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="profile__nav">
+    <ul>
+      <li><a href="#relations">Relations</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#elsewhere">Find me elsewhere</a></li>
+      <li><a href="#access-groups">Access groups</a></li>
+      <li><a href="#vouches">Vouches</a></li>
+      <li><a href="#tags">Tags</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'ProfileNav',
+};
+</script>
+
+<style>
+  .profile__nav {
+    grid-column: 1 / 2;
+    grid-row: 2;
+    margin-bottom: 2em;
+    position: absolute;
+    left: -9999em;
+    top: -9999em;
+  }
+    .profile__nav ul {
+      padding: 0;
+      margin: 0;
+    }
+    .profile__nav li {
+      list-style: none;
+    }
+    .profile__nav a {
+      display: block;
+      padding: 1em;
+      background: var(--white);
+      text-decoration: none;
+      border-bottom: 1px solid var(--lightGrey);
+      color: var(--darkGrey);
+    }
+      .profile__nav a:hover {
+        color: var(--blue);
+        border-bottom: 1px solid currentColor;
+      }
+  @media(min-width:50em) {
+    .profile__nav {
+      position: sticky;
+      top: 0;
+    }
+  }
+</style>
