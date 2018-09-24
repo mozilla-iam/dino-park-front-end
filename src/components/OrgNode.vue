2fe90be4f5801ff9219869ff151091c8993f20c8
Florian Merz <flomerz@gmail.com>
2018-09-24 16:56:46 +0200

    proper ul > li

--- src/components/OrgNode.vue
+++ src/components/OrgNode.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="org-node">
-    <input :id="`${prefix}-${id}`" type="checkbox">
-    <li v-bind:class="{ branch: children.length }">
-      <div class="toggle">
+    <li>
+      <input :id="`${prefix}-${id}`" type="checkbox">
+      <span v-bind:class="{ 'org-node__leaf': (children.length === 0) }">
         <label :for="`${prefix}-${id}`">
           <a :href="`/profile/${data.user_id}`">
             {{ data.first_name }} {{ data.last_name }}
@@ -11,7 +11,7 @@
         <ul v-for="(child, index) in children" :key="index">
           <OrgNode :children="child.children" :data="child.data" :id="index" :prefix="`${prefix}-${id}`"></OrgNode>
         </ul>
-      </div>
+      </span>
     </li>
   </div>
 </template>
@@ -29,43 +29,41 @@ export default {
 </script>
 
 <style>
-  .org-node label {
-    width: 100%;
-  }
+.org-node span {
+  width: 100%;
+}
 
-  .org-node label:hover {
-      background-color: lightgray;
-  }
+.org-node label:hover {
+  background-color: lightgray;
+}
 
-  .org-node span {
-      color: #4F7FA4;
-  }
+.org-node > li > input:checked + span::before {
+  content: "▶";
+  float: left;
+}
 
-  .org-node input {
-      position: absolute;
-      opacity: 0;
-      cursor: pointer;
-  }
+.org-node span::before {
+  content: "▼";
+  float: left;
+}
 
-  .org-node li {
-      list-style: disc;
-      display: list-item;
-  }
+.org-node > li > input + span.org-node__leaf::before {
+  content: "●";
+  float: left;
+}
 
-  .org-node input:checked + li {
-      list-style-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAALCAYAAABCm8wlAAAAlklEQVQYlWPonDx/VXv7NEEGXKBz8oL/HZMXvGufuCANpwIY7pi0YHd7/zwlnAo6Jy/43zll4bv2SfPLcStANw2XgllL1r/bsvOwMYaCrikL/+85dPL/k6cvdj98+BLVhAUrNv2/fvvBu8dPn6O6oWvKwv8nzlxC6EIGy9Zu/3/v4eN3T548D8UaDk+evVz18OFDnCEJAEBcmet8woHKAAAAAElFTkSuQmCC);
-  }
+.org-node input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  display: initial;
+}
 
-  .org-node li.branch {
-      list-style-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAJCAYAAADgkQYQAAAApUlEQVQYlWNonzS/vHPygv+4cPuk+eUMj5+/KZ86bxVWBVPnrfr/+PmbcgYGBgaGE+evvMOm6MDRs+8YYODJ81ehC1ZuRlGwYOXm/09evk5jQAbX7zw4g6zoxLmrZxjQwcOXL5W2bt37v3Pygv9rNu/9//TlSxcMRQwMDAx37j+e2T9j6f8rN+6twqqAgYGB4eHDh4LX7jx49/DlSyWcimDWoosBAHcwjGsLIU2wAAAAAElFTkSuQmCC);
-  }
+.org-node > li {
+  list-style: none;
+}
 
-
-  .org-node label + .toggle + ul {
-      display: inline-block;
-  }
-
-  .org-node input:checked + li ul {
-      display: none;
-  }
+.org-node > li > input:checked + span ul {
+  display: none;
+}
 </style>
