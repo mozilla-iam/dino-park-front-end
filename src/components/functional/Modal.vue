fd1cd5eac129372efc0b5218b2c31104a09837c4
Hidde de Vries <hidde@hiddedevries.nl>
2018-10-11 15:35:29 +0200

    Make modal darker
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/functional/Modal.vue
+++ src/components/functional/Modal.vue
@@ -79,7 +79,7 @@ export default {
     width: 100%;
     height: 100%;
     position: fixed;
-    background: rgba(0,0,0, .5);
+    background: rgba(0,0,0, .7);
     display: flex;
     align-items: center;
     padding: 1em;
