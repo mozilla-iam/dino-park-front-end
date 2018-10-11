7389838e066b1447187524fffc32e990028e958a
Michael Kohler <me@michaelkohler.info>
2018-10-11 21:58:47 +0200

    Remove non-functional PersonList on Home page

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,42 +1,20 @@
 <template>
-  <ApolloQuery :query="profileList">
-    <template slot-scope="{ result: { loading, data, error } }">
-      <LoadingSpinner v-if="loading"></LoadingSpinner>
-      <template v-else-if="error">An error occured</template>
-      <template v-else-if="data">
-        <main class="container home">
-          <div class="intro">
-            <h1>DinoPark</h1>
-            <p>Welcome to the Mozilla directory</p>
-            <p>This site lists all core individuals and groups that participate in the Mozilla Project. We call ourselves Mozillians and this is where we connect and share with one another.</p>
-          </div>
-          <PersonList v-if="data.profiles">
-            <Person v-for="profile in data.profiles" v-bind="profile" :key="profile.index" modifier="person--wide"></Person>
-          </PersonList>
-        </main>
-      </template>
-      <LoadingSpinner v-else></LoadingSpinner>
-    </template>
-  </ApolloQuery>
+  <main class="container home">
+    <div class="intro">
+      <h1>DinoPark</h1>
+      <p>Welcome to the Mozilla directory</p>
+      <p>This site lists all core individuals and groups that participate in the Mozilla Project. We call ourselves Mozillians and this is where we connect and share with one another.</p>
+    </div>
+  </main>
 </template>
 
 <script>
-import Person from '@/components/Person.vue';
-import PersonList from '@/components/PersonList.vue';
 import LoadingSpinner from '@/components/LoadingSpinner.vue';
-import { PROFILE_LIST } from '@/queries/profile';
 
 export default {
   name: 'PageHome',
   components: {
-    Person,
-    PersonList,
     LoadingSpinner,
-  },
-  data() {
-    return {
-      profileList: PROFILE_LIST,
-    };
   },
 };
 </script>
