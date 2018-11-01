46770dbb6c3d5d2f5434d4e25f042457d3cbb5d8
Hidde de Vries <hidde@hiddedevries.nl>
2018-11-01 14:34:19 +0100

    Use IntersectionObserver to set current profilenav
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/ProfileNav.vue
+++ src/components/ProfileNav.vue
@@ -26,6 +26,31 @@ export default {
   components: {
     Icon,
   },
+  methods: {
+    watchCurrentSections() {
+      const sections = this.links.map(link => { return document.getElementById(link.id).parentNode; });
+      const observer = new IntersectionObserver(this.updateCurrent);
+      
+      sections.forEach(section => {
+        observer.observe(section);
+      });
+    },
+    updateCurrent(entries, observer) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.current = entry.target.firstChild.id;
+        }
+      });
+    },
+  },
+  mounted() {
+    this.watchCurrentSections();
+  },
+  data() {
+    return {
+      current: null,
+    }
+  },
 };
 </script>
 
@@ -63,38 +88,43 @@ export default {
     list-style: none;
     flex: 1;
   }
-  .profile__nav a {
+  .profile__nav-link {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
+    justify-content: center;
     height: 100%;
-    padding: 1.35em;
+    padding: 1.35em .5em;
     background: var(--white);
     text-decoration: none;
     border-bottom: 2px solid var(--gray-20);
+    border-left: 1px solid var(--grey-30);
     color: var(--gray-50);
     text-align: center;
-    font-size: .9em;
+    font-size: .75em;
   }
-  .profile__nav a:hover {
+  li:first-child .profile__nav-link {
+    border-left: 0;
+  }
+  .profile__nav-link:hover {
     color: var(--blue-60);
     border-bottom: 2px solid currentColor;
   }
   @media(min-width:50em) {
-    .profile__nav a {
+    .profile__nav-link {
       text-align: left;
       flex-direction: row;
       justify-content: start;
       padding: 1.5em;
+      font-size: .9em;
     }
   }
-    .profile__nav a svg {
+    .profile__nav-link svg {
       order: 2;
       margin-top: .5em;
     }
     @media (min-width:50em) {
-      .profile__nav a svg {
+      .profile__nav-link svg {
         order: 0;
         margin-top: 0;
         margin-right: 1em;
@@ -108,5 +138,11 @@ export default {
     .profile__nav-label {
       position: static;
     }
+  }
+  .profile__nav-link--current {
+    color: var(--blue-60);
+  }
+  .profile__nav-link--current .profile__nav-label {
+    position: static;
   }
 </style>
