073a3d9f6ae2a27336ff40e1d1e1eebc2e3e8e5b
Florian Merz <flomerz@gmail.com>
2018-11-08 20:04:06 +0100

    Add Picture component with identicons.

---
+++ src/components/Picture.vue
@@ -0,0 +1,56 @@
+<template>
+  <img :class="cls" :src="src" alt="" :width="size" :title="title" role="presentation" aria-hidden="true">
+</template>
+
+
+<script>
+import Identicon from 'identicon.js';
+
+function hex(buffer) {
+  const hexCodes = [];
+  const view = new DataView(buffer);
+  for (let i = 0; i < view.byteLength; i += 4) {
+    const value = view.getUint32(i);
+    const stringValue = value.toString(16);
+    const padding = '00000000';
+    const paddedValue = (padding + stringValue).slice(-padding.length);
+    hexCodes.push(paddedValue);
+  }
+  return hexCodes.join('');
+}
+
+function sha256(str) {
+  const buffer = new TextEncoder('utf-8').encode(str);
+  return crypto.subtle.digest('SHA-256', buffer).then(hex);
+}
+
+export default {
+  name: 'Picture',
+  props: {
+    size: String,
+    picture: String,
+    username: String,
+    title: String,
+    cls: String,
+  },
+  watch: {
+    username() {
+      this.updatePicture();
+    },
+  },
+  data() {
+    this.updatePicture();
+    return { src: this.picture };
+  },
+  methods: {
+    updatePicture() {
+      if (this.picture === null) {
+        sha256(this.username).then((hash) => {
+          const identicon = new Identicon(hash, { size: this.size, format: 'svg' });
+          this.src = `data:image/svg+xml;base64,${identicon.toString()}`;
+        });
+      }
+    },
+  },
+};
+</script>
