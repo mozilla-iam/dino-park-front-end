7560be6e8cb14f50756a53f715f831c7fe184fe6
Florian Merz <flomerz@gmail.com>
2018-11-08 20:04:06 +0100

    Add Picture component with identicons.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,7 +3,7 @@
     <section class="profile__section profile__intro">
       <div class="profile__intro-photo">
         <div class="profile__headshot">
-          <img :src="picture.value" alt="" />
+          <Picture :picture="picture.value" :username="userId.value" size="230"></Picture>
         </div>
         <div class="hide-mobile">
           <ContactMe></ContactMe>
@@ -118,7 +118,7 @@
         <h2>Access Groups</h2>
       </header>
       <ul class="relation-list">
-        <Relation category="NDA" />
+        <Relation v-for="(group, index) in accessInformation.mozilliansorg.values" :key="`group-${index}`" :category="group" />
       </ul>
     </section>
     <section class="profile__section">
@@ -141,6 +141,7 @@ import Meta from '@/components/Meta.vue';
 import MetaList from '@/components/MetaList.vue';
 import Modal from '@/components/functional/Modal.vue';
 import Person from '@/components/Person.vue';
+import Picture from '@/components/Picture.vue';
 import ProfileName from '@/components/ProfileName.vue';
 import ProfileTitle from '@/components/ProfileTitle.vue';
 import ProfileTeamLocation from '@/components/ProfileTeamLocation.vue';
@@ -195,6 +196,7 @@ export default {
     MetaList,
     Modal,
     Person,
+    Picture,
     ProfileName,
     ProfileNav,
     ProfileTeamLocation,
