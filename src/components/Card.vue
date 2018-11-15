1602bd1845521cf955863ece82b4fa25411752b7
Hidde de Vries <hidde@hiddedevries.nl>
2018-11-15 17:32:18 +0100

    Add first version of logged in landing page
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="'card' + ( modifier ? ' ' + modifier : '')">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    modifier: String,
+  },
+};
+</script>
+
+
+<style>
+  .card {
+    background-color: var(--white);
+    box-shadow: var(--shadowCard);
+    padding: 1em;
+  }
+  .card--centered-content {
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+    .card > a {
+      color: inherit;
+      text-decoration: none;
+    }
+</style>
---
+++ src/components/CardRow.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardRow',
+};
+</script>
+
+
+<style>
+  .card-row {
+    overflow-x: auto;
+    max-width: 100%;
+    white-space: nowrap;
+    display: flex;
+  }
+    .card-row .card {
+      display: inline-flex;
+      flex-direction: column;
+      margin-right: 2em;
+      margin-bottom: 1em;
+      width: 22.666em;
+      flex: none;
+      white-space: normal;
+    }
+</style>
