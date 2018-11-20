6fd94326f09e6398e9c40567a4ac8d4ae91436cf
Hidde de Vries <hidde@hiddedevries.nl>
2018-11-20 16:22:50 +0100

    Add hover state to cards (DP-733)
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/CardRow.vue
+++ src/components/CardRow.vue
@@ -18,6 +18,10 @@ export default {
     max-width: calc(100vw - 2em);
     white-space: nowrap;
     display: flex;
+    padding-top: .25em; /* allow for box shadow */
+    padding-left: .25em;
+    margin-top: -.25em;
+    margin-left: -.25em;
   }
     .card-row .card {
       display: inline-flex;
