9f7f952745f51eb94504e930727f2dbcf502b1a1
Hidde de Vries <hidde@hiddedevries.nl>
2018-11-29 13:47:02 +0100

    Improve mobile error message
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -16,18 +16,31 @@ export default {
 </script>
 
 <style>
+.error__image {
+  margin: 2em 0;
+  text-align: center;
+}
+  .error__image img {
+    max-width: 80%;
+  }
+  .error__message > .button {
+    margin: 4em 0;
+    display: inline-block;
+  }
 @media (min-width: 57.5em) {
   .error {
     display: flex;
     margin: 4em;
   }
+    .error__image {
+      margin: 0;
+    }
+      .error__image img {
+        max-width: none;
+      }
     .error__image,
     .error__message {
       flex: 1;
-    }
-    .error__message > .button {
-      margin: 4em 0;
-      display: inline-block;
     }
 }
 </style>
