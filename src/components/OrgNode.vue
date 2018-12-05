d719d6c0210134ede01aafe2f216c45aa7bd44f6
Hidde de Vries <hidde@hiddedevries.nl>
2018-12-05 11:18:31 +0100

    Add fallback for browsers that don't support scrollIntoView object syntax
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/OrgNode.vue
+++ src/components/OrgNode.vue
@@ -58,9 +58,16 @@ export default {
     if (this.data.username === this.$route.params.username) {
       const e = document.getElementById(`${this.data.username}`);
       if (e) {
-        e.scrollIntoView({
-          block: 'center',
-        });
+        // assume browsers with `scrollBehavior` support scrollIntoView
+        // with object as arguments
+        if ('scrollBehavior' in document.documentElement.style) {
+          e.scrollIntoView({
+            block: 'center',
+          });
+        } else {
+          // scrolls into view with element at bottom of viewport (block: 'end')
+          e.scrollIntoView(false);
+        }
       }
     }
   },
