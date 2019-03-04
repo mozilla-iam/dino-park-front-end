d5e836f7846a03745b707b2e40081e19096f4a2b
Hidde de Vries <hidde@hiddedevries.nl>
2019-03-04 17:03:59 +0100

    Add combobox and use for pronouns, location
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

---
+++ src/components/Combobox.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="combobox">
+    <input
+      type="text"
+      :id="id"
+      class="combobox__input"
+      ref="comboboxInput"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+    <ul v-if="source" class="combobox__options" ref="comboboxList">
+      <li
+        v-for="(option, index) in source"
+        :key="index"
+        class="combobox__option"
+      >
+        {{ option }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Combobo from 'combobo';
+
+export default {
+  name: 'Autocomplete',
+  props: {
+    id: String,
+    source: Array,
+    value: String,
+    initialValue: String,
+  },
+  methods: {
+    init() {
+      const { comboboxInput, comboboxList } = this.$refs;
+
+      this.combobo = new Combobo({
+        input: comboboxInput,
+        list: comboboxList,
+        options: this.optionEls,
+        activeClass: 'combobox__option--selected',
+      });
+
+      this.combobo.addEventListener('selection', () => {
+        this.$nextTick(() => {
+          this.$emit('input', this.$refs.comboboxInput.value);
+        });
+      });
+    },
+  },
+  mounted() {
+    this.init();
+  },
+  data() {
+    return {
+      combobo: null,
+    };
+  },
+  computed: {
+    optionEls() {
+      return Array.from(
+        this.$refs.comboboxList.querySelectorAll('.combobox__option'),
+      );
+    },
+  },
+};
+</script>
+
+<style>
+.combobox__input[aria-expanded='false'] + .combobox__options {
+  display: none;
+}
+.combobox__input[aria-expanded='true'] + .combobox__options {
+  display: block;
+}
+.combobox {
+  position: relative;
+}
+.combobox__options {
+  padding-left: 0;
+  width: 100%;
+  position: absolute;
+  top: 3em;
+  margin: 0;
+  background-color: var(--white);
+  box-shadow: 0 0 0.25em 0 var(--gray-30);
+}
+.combobox__option {
+  list-style: none;
+  padding: 0.5em 1em;
+}
+.combobox__option--selected {
+  background-color: var(--blue-60);
+  color: var(--white);
+}
+</style>
--- src/components/forms/EditPersonalInfo.vue
+++ src/components/forms/EditPersonalInfo.vue
@@ -52,36 +52,15 @@
 
           <hr role="presentation" />
 
-          <div class="edit-personal-info__label">Gender pronouns</div>
-          <Select
-            label="Select pronoun"
-            id="field-pronouns"
-            class="options--chevron"
-            v-model="pronouns.value"
-            :options="[
-              {
-                label: 'Select pronoun',
-                value: '',
-              },
-              {
-                label: 'He/Him',
-                value: 'He/him',
-              },
-              {
-                label: 'She/Her',
-                value: 'She/her',
-              },
-              {
-                label: 'They/Them',
-                value: 'They/Them',
-              },
-              {
-                label: 'Ze/Hir',
-                value: 'Ze/Hir',
-              },
-            ]"
+          <label for="field-pronouns" class="edit-personal-info__label"
+            >Gender pronouns</label
           >
-          </Select>
+          <Combobox
+            id="field-pronouns"
+            v-model="pronouns.value"
+            :source="['He/Him', 'She/Her', 'They/Them']"
+          >
+          </Combobox>
           <div class="edit-personal-info__privacy">
             <Select
               label="Pronoun privacy levels"
@@ -135,7 +114,19 @@
           <hr role="presentation" />
 
           <label for="field-location">Location</label>
-          <input type="text" id="field-location" v-model="location.value" />
+          <Combobox
+            id="field-location"
+            v-model="location.value"
+            :source="[
+              'Berlin',
+              'Rotterdam',
+              'San Francisco',
+              'Cluj-Napoca',
+              'München',
+            ]"
+          >
+          </Combobox>
+
           <div class="edit-personal-info__privacy">
             <Select
               label="Location privacy levels"
@@ -194,6 +185,7 @@
 </template>
 
 <script>
+import Combobox from '@/components/Combobox.vue';
 import Select from '@/components/Select.vue';
 
 import { MUTATE_PROFILE, DISPLAY_PROFILE } from '@/queries/profile';
@@ -206,6 +198,7 @@ export default {
     initialValues: Object,
   },
   components: {
+    Combobox,
     Select,
   },
   methods: {
@@ -295,6 +288,7 @@ textarea {
   margin: 1em -2.4em;
 }
 .edit-personal-info > input,
+.edit-personal-info > div > input,
 .edit-personal-info > textarea,
 .edit-personal-info > select {
   border: 0;
