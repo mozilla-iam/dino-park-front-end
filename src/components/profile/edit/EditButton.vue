acb0b2f2dd8a647f0789e26f177a9be36e67a0ad
Hidde de Vries <hidde@hiddedevries.nl>
2019-04-05 12:12:12 +0200

    Componentise edit button
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

---
+++ src/components/profile/edit/EditButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="profile__edit-button"
+    @click="
+      $router.push({
+        name: 'Edit Profile',
+        query: {
+          section: sectionId,
+        },
+      })
+    "
+  >
+    <img src="@/assets/images/icon-pencil.svg" :alt="`Edit ${section}`" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'EditButton',
+  props: {
+    section: String,
+    sectionId: String,
+  },
+};
+</script>
