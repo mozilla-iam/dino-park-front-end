e8393d8ab4e32d8df5fe434fd605d50ad10f1e64
Hidde de Vries <hidde@hiddedevries.nl>
2019-04-05 13:07:23 +0200

    Use Icon component for button
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/profile/edit/EditButton.vue
+++ src/components/profile/edit/EditButton.vue
@@ -1,6 +1,6 @@
 <template>
   <RouterLink
-    class="profile__edit-button"
+    class="edit-button"
     :to="{
       name: 'Edit Profile',
       query: {
@@ -8,16 +8,43 @@
       },
     }"
   >
-    <img src="@/assets/images/icon-pencil.svg" :alt="`Edit ${section}`" />
+    <Icon id="pencil" :width="20" :height="20"></Icon>
+    <span class="visually-hidden">Edit {{ section }}</span>
   </RouterLink>
 </template>
 
 <script>
+import Icon from '@/components/ui/Icon.vue';
+
 export default {
   name: 'EditButton',
   props: {
     section: String,
     sectionId: String,
   },
+  components: {
+    Icon,
+  },
 };
 </script>
+
+<style>
+.edit-button {
+  border: none;
+  background: none;
+  color: var(--black);
+}
+.edit-button:hover {
+  color: var(--blue-60);
+}
+.profile__section-header .edit-button {
+  position: absolute;
+  top: 1.5em;
+  right: 1.5em;
+}
+.profile__intro .edit-button {
+  position: absolute;
+  top: 1em;
+  right: 0;
+}
+</style>
