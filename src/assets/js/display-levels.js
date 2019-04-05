834f4a4b56495dbe9029f2653a77a40810f28d0d
Florian Merz <flomerz@gmail.com>
2019-04-05 14:45:48 +0200

    first/last name must not be private

--- src/assets/js/display-levels.js
+++ src/assets/js/display-levels.js
@@ -34,6 +34,13 @@ export const DISPLAY_LEVELS = {
 const DISPLAY_PUBLIC_ONLY = [DISPLAY_LEVELS.public];
 const DISPLAY_STAFF_NDAED_ONLY = [DISPLAY_LEVELS.ndaed, DISPLAY_LEVELS.staff];
 const DISPLAY_STAFF_ONLY = [DISPLAY_LEVELS.staff];
+const DISPLAY_NOT_PRIVATE = [
+  DISPLAY_LEVELS.staff,
+  DISPLAY_LEVELS.ndaed,
+  DISPLAY_LEVELS.vouched,
+  DISPLAY_LEVELS.authenticated,
+  DISPLAY_LEVELS.public,
+];
 const DISPLAY_ANY = [
   DISPLAY_LEVELS.private,
   DISPLAY_LEVELS.staff,
@@ -45,6 +52,8 @@ const DISPLAY_ANY = [
 
 const VALID_DISPLAY_LEVELS = {
   primaryUsername: DISPLAY_PUBLIC_ONLY,
+  firstName: DISPLAY_NOT_PRIVATE,
+  lastName: DISPLAY_NOT_PRIVATE,
   'staffInformation.manager': DISPLAY_STAFF_NDAED_ONLY,
   'staffInformation.director': DISPLAY_STAFF_NDAED_ONLY,
   'staffInformation.staff': DISPLAY_STAFF_NDAED_ONLY,
