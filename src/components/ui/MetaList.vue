1b5a5c4582773240ff3b1c940d494aa17bacb20d
Hidde de Vries <hidde@hiddedevries.nl>
2019-04-05 17:01:37 +0200

    Add and responsively style staff info on edit card
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/ui/MetaList.vue
+++ src/components/ui/MetaList.vue
@@ -19,5 +19,30 @@ export default {
   overflow: hidden;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1em 2em;
+}
+.edit-personal-info__meta .meta-list {
+  grid-template-columns: auto;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  color: var(--gray-50);
+}
+.edit-personal-info__meta .meta-list dt {
+  font-weight: 400;
+  margin: 0.125em 0;
+}
+@media (min-width: 32em) {
+  .edit-personal-info__meta .meta-list {
+    grid-template-columns: repeat(2, auto);
+  }
+  .edit-personal-info__meta .meta-list dl {
+    display: contents; /* with multiple columns, lay out not this dl, but its children */
+  }
+}
+@media (min-width: 42em) {
+  .edit-personal-info__meta .meta-list {
+    grid-template-columns: repeat(4, auto);
+  }
 }
 </style>
