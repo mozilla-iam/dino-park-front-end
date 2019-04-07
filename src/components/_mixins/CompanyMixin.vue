c45772d214daef5d4aab4c9f5424362e96aa59eb
Florian Merz <flomerz@gmail.com>
2019-04-07 16:56:57 +0200

    community ldap is not MoCo

--- src/components/_mixins/CompanyMixin.vue
+++ src/components/_mixins/CompanyMixin.vue
@@ -2,7 +2,7 @@
 export default {
   methods: {
     company(staffInformation, primaryEmail) {
-      if (staffInformation && staffInformation.staff && primaryEmail) {
+      if (staffInformation && staffInformation.staff.value && primaryEmail) {
         if (primaryEmail.endsWith('mozillafoundation.org')) {
           return 'Mozilla Foundation';
         }
