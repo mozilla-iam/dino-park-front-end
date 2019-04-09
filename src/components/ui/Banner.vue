2fcab1d1e56b129133ef30d2de07a4688e05b12e
Hidde de Vries <hidde@hiddedevries.nl>
2019-04-09 11:17:55 +0200

    Add banner (DP-1105)
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

---
+++ src/components/ui/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="banner">
+    <div class="container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+};
+</script>
+
+<style>
+.banner {
+  background-color: var(--black);
+  color: var(--white);
+  text-align: center;
+}
+@media (min-width: 57.5em) {
+  .banner {
+    padding: 0.5em;
+  }
+}
+.banner a {
+  color: var(--gray-10);
+}
+.banner a:hover {
+  color: inherit;
+}
+.focus-styles .banner a:focus {
+  box-shadow: 0px 0 0 1px var(--white), 0 0 0 3px var(--transparentWhite);
+}
+</style>
