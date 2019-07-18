61ae370d98eefa636e819868cacd969dc5ea9df8
Andrew Sunada <asunada@mozilla.com>
2019-07-18 13:30:47 -0700

    feat(dino-park): setting up jest testing

---
+++ src/components/_mixins/AccountsMixin.spec.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import AccountsMixin from './AccountsMixin.vue';
+
+describe('AccountsMixin', () => {
+  it('renders basically', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/components/ui/Tag.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils';
+import Tag from './Tag.vue';
+
+describe('Tag.vue', () => {
+  it('renders props.name when passed', () => {
+    expect(true).toBe(true);
+  });
+});
