065f281fb8a3e125e9ae67c3758714a365ce314a
Florian Merz <flomerz@gmail.com>
2019-08-06 15:54:59 +0200

    Link desk number to office plans.

---
+++ src/components/_mixins/OfficesMixin.vue
@@ -0,0 +1,30 @@
+<script>
+const OFFICES = {
+  BER: 'Berlin+Office+Floor+plan+-+BER',
+  LON: 'London+Office+Floor+plan+-+LON',
+  MTV: 'Mountain+View+Office+Floor+plans+-+MTV',
+  PAR: 'Paris+Office+Floor+plans+-+PAR',
+  PDX: 'Portland+Office+Floor+plan+-+PDX',
+  SFO: 'San+Francisco+Office+Floor+plans+-+SFO',
+  TPE: 'Taipei+Office+Floor+plan+-+TPE',
+  TOR: 'Toronto+Office+Floor+plan-+TOR',
+  VVR: 'Vancouver+Office+Floor+plan+-+YVR',
+};
+
+export default {
+  methods: {
+    deskNumberToOfficePlanLink(deskNumber) {
+      if (typeof deskNumber === 'string') {
+        const handle = deskNumber.startsWith('MV')
+          ? 'MTV'
+          : deskNumber.slice(0, 3);
+        const name = handle && OFFICES[handle];
+        if (name) {
+          return `https://mana.mozilla.org/wiki/display/WPR/${name}`;
+        }
+      }
+      return null;
+    },
+  },
+};
+</script>
