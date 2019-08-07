a3b6386413a6d19ea7f44ae94d06bf6c3dc5752a
Florian Merz <flomerz@gmail.com>
2019-08-07 14:55:21 +0200

    Fix prefixes for desk → office links.

--- src/components/_mixins/OfficesMixin.vue
+++ src/components/_mixins/OfficesMixin.vue
@@ -5,20 +5,18 @@ const OFFICES = {
   MTV: 'Mountain+View+Office+Floor+plans+-+MTV',
   PAR: 'Paris+Office+Floor+plans+-+PAR',
   PDX: 'Portland+Office+Floor+plan+-+PDX',
-  SFO: 'San+Francisco+Office+Floor+plans+-+SFO',
+  SF: 'San+Francisco+Office+Floor+plans+-+SFO',
   TPE: 'Taipei+Office+Floor+plan+-+TPE',
   TOR: 'Toronto+Office+Floor+plan-+TOR',
-  VVR: 'Vancouver+Office+Floor+plan+-+YVR',
+  YVR: 'Vancouver+Office+Floor+plan+-+YVR',
 };
 
 export default {
   methods: {
     deskNumberToOfficePlanLink(deskNumber) {
       if (typeof deskNumber === 'string') {
-        const handle = deskNumber.startsWith('MV')
-          ? 'MTV'
-          : deskNumber.slice(0, 3);
-        const name = handle && OFFICES[handle];
+        const name =
+          OFFICES[deskNumber.slice(0, 3)] || OFFICES[deskNumber.slice(0, 2)];
         if (name) {
           return `https://mana.mozilla.org/wiki/display/WPR/${name}`;
         }
