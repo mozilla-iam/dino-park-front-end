b4b0a6a94660f98d7751bd83c041144b3569bc45
Andrew Sunada <asunada@mozilla.com>
2019-10-31 15:02:34 -0700

    chore(IAM-293): adding access-groups page, panelsection, moving styles from profile to profilesection

--- src/components/profile/ProfileSection.vue
+++ src/components/profile/ProfileSection.vue
@@ -70,3 +70,83 @@ export default {
   },
 };
 </script>
+
+<style>
+.profile__section {
+  border: 4px solid var(--gray-50);
+  background: #fff;
+  padding: 1.5em;
+  margin: 0 0 2em;
+  grid-column: 2 / -1;
+  overflow: visible;
+  border-radius: var(--cardRadius);
+  position: relative;
+}
+
+.profile__section:not(.profile__section--editing):not(.first) {
+  padding: calc(2px + 1.5em);
+}
+
+.profile__section.first {
+  padding: 0;
+}
+
+.profile__section.first .profile__section-header {
+  margin: 0 0 1.5em 0;
+}
+@supports (--key: value) {
+  .profile__section {
+    border: none;
+    box-shadow: var(--shadowCard);
+  }
+}
+.profile__section--disabled {
+  background-color: var(--gray-20);
+  color: var(--gray-50);
+  border: 2px solid var(--gray-30);
+}
+.profile__section.profile__section--editing {
+  border: 2px solid var(--blue-60);
+}
+.profile__section:first-child {
+  grid-column: 1 / -1;
+}
+
+.profile__section .reporting-structure h3 {
+  margin-top: 0;
+}
+
+.profile__section-header {
+  padding: 1.5em;
+  margin: -1.5em -1.5em 1.5em -1.5em;
+  border-bottom: 1px solid var(--gray-30);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.profile__section:not(.profile__section--editing):not(.first)
+  .profile__section-header {
+  margin: calc(-1.5em - 2px) calc(-1.5em - 2px) calc(1.5em + 2px)
+    calc(-1.5em - 2px);
+  padding-top: calc(1.5em + 2px);
+  padding-left: calc(1.5em + 2px);
+  padding-right: calc(1.5em + 2px);
+}
+.profile__section-header h2 {
+  margin: 0 0 0.75em 0;
+  width: 100%;
+}
+@media (min-width: 35em) {
+  .profile__section-header h2 {
+    margin: 0;
+    width: auto;
+  }
+}
+.profile__section--disabled {
+  background-color: var(--gray-20);
+  color: var(--gray-50);
+  border: 2px solid var(--gray-30);
+}
+</style>
---
+++ src/components/ui/PanelSection.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="panel__section">
+    <header class="panel__section-header">
+      <h2>{{ title }}</h2>
+      <slot name="header-content"></slot>
+    </header>
+    <div :class="{ 'panel__section-content': true, full: fullContent }">
+      <slot name="view"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PanelSection',
+  props: {
+    fullContent: {
+      type: Boolean,
+      default: false,
+    },
+    title: String,
+  },
+};
+</script>
+
+<style>
+.panel__section {
+  border: 4px solid var(--gray-50);
+  background: var(--white);
+  margin: 0 0 2em;
+  grid-column: 2 / -1;
+  overflow: visible;
+  border-radius: var(--cardRadius);
+  position: relative;
+}
+
+.panel__section.first {
+  padding: 0;
+}
+
+.panel__section.first .panel__section-header {
+  margin: 0 0 1.5em 0;
+}
+@supports (--key: value) {
+  .panel__section {
+    border: none;
+    box-shadow: var(--shadowCard);
+  }
+}
+.panel__section:first-child {
+  grid-column: 1 / -1;
+}
+
+.panel__section .reporting-structure h3 {
+  margin-top: 0;
+}
+
+.panel__section-header {
+  padding: 1.5em;
+  border-bottom: 1px solid var(--gray-30);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.panel__section-content.full {
+  padding: 1.5em;
+}
+
+.panel__section-header h2 {
+  margin: 0 0 0.75em 0;
+  width: 100%;
+}
+@media (min-width: 35em) {
+  .panel__section-header h2 {
+    margin: 0;
+    width: auto;
+  }
+}
+</style>
