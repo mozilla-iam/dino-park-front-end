54751de4a606871651d6aac3e12fca139fce9888
Andrew Sunada <asunada@mozilla.com>
2019-11-06 14:01:26 -0800

    feat(IAM-341): adding initial membership management component

---
+++ src/components/access_group/AccessGroupMembershipManagement.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="membership-management-container">
+    <h1>here</h1>
+  </article>
+</template>
+
+<script>
+import Icon from '@/components/ui/Icon.vue';
+
+export default {
+  name: 'AccessGroupMembershipManagement',
+  components: { Icon },
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style></style>
