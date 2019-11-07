4d145bb529e46666f2879478304071b15c2dfaf6
Andrew Sunada <asunada@mozilla.com>
2019-11-06 17:24:40 -0800

    feat(IAM-342): adding curators

--- src/components/access_group/AccessGroupCurators.vue
+++ src/components/access_group/AccessGroupCurators.vue
@@ -1,14 +1,18 @@
 <template>
   <ul :class="{ 'curators-list': true, tiled: curators.length > 3 }">
-    <li v-for="(curator, idx) in curators" class="curators-list__item" :key="idx">
+    <li
+      v-for="(curator, idx) in curators"
+      class="curators-list__item"
+      :key="idx"
+    >
       <UserPicture
-        :avatar="{ picture: curator.picture, username: curator.name }"
+        :avatar="{ picture: curator.imageUrl, username: curator.name }"
         class="curator-image"
         :size="40"
         :isStaff="true"
       />
       <p class="curator-name">{{ curator.name }}</p>
-      <a :href="`mailto:${curator.email}`" class="curator-email">
+      <a href="#" class="curator-email">
         <Icon id="email" :width="16" :height="16" />
       </a>
     </li>
@@ -29,10 +33,11 @@ export default {
       type: Boolean,
       default: true,
     },
+    curatorsList: Array,
   },
   computed: {
     curators() {
-      return this.$store.state.accessGroup.curators;
+      return this.curatorsList;
     },
   },
   data() {
@@ -51,87 +56,48 @@ export default {
   width: 100%;
 }
 
-.curators-list .curators-list__item {
+.curators-list:not(.tiled) .curators-list__item {
   width: 100%;
   display: flex;
   flex-direction: row;
   margin: 0.5em 0;
 }
 
-.curators-list .curators-list__item:first-child {
+.curators-list:not(.tiled) .curators-list__item:first-child {
   margin-top: 0;
 }
 
-.curators-list .curators-list__item:last-child {
+.curators-list:not(.tiled) .curators-list__item:last-child {
   margin-bottom: 0;
 }
 
-.curators-list .curators-list__item:nth-child(odd) {
+.curators-list:not(.tiled) .curators-list__item:nth-child(odd) {
   background: var(--gray-10);
 }
 
-.curators-list .curators-list__item .curator-image {
+.curators-list:not(.tiled) .curators-list__item .curator-image {
   width: 10%;
 }
 
-.curators-list .curators-list__item .curator-image {
+.curators-list:not(.tiled) .curators-list__item .curator-email {
+  width: 20%;
+}
+
+.curators-list:not(.tiled) .curators-list__item .curator-image {
   margin: 0.5em 1em;
 }
 
-.curators-list .curators-list__item .curator-email {
-  display: none;
-}
-.curators-list .curators-list__item .curator-name {
-  width: 80%;
-  margin: 0.6em 0;
+.curators-list:not(.tiled) .curators-list__item .curator-email {
   display: flex;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
 }
-
-@media (min-width: 35em) {
-  .curators-list:not(.tiled) .curators-list__item .curator-email {
-    width: 20%;
-  }
-
-  .curators-list:not(.tiled) .curators-list__item .curator-email {
-    display: flex;
-    text-align: center;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .curators-list.tiled {
-    display: grid;
-    grid-template-columns: 20% 20% 20% 20% 20%;
-    grid-row-gap: 1em;
-  }
-
-  .curators-list.tiled .curators-list__item {
-    width: 5em;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 0;
-  }
-
-  .curators-list.tiled .curators-list__item:nth-child(6n) {
-    flex-basis: 100%;
-  }
-  .curators-list.tiled .curators-list__item:nth-child(odd) {
-    background: initial;
-  }
-  .curators-list.tiled .curators-list__item .curator-name {
-    display: none;
-  }
-
-  .curators-list.tiled .curators-list__item .curator-email {
-    margin-top: 0.5em;
-  }
-
-  .curators-list.tiled .curators-list__item .curator-image {
-    width: auto;
-  }
+.curators-list:not(.tiled) .curators-list__item .curator-name {
+  width: 80%;
+  margin: 0.6em 1em;
+  display: flex;
+  align-items: center;
 }
 </style>
