f8213dff3c42b1ba5d63ef544ec3243a9a1a86ce
Andrew Sunada <asunada@mozilla.com>
2019-11-20 15:22:03 -0800

    fix(IAM-191): updating icons for edit page tabs

--- src/components/ui/Icon.vue
+++ src/components/ui/Icon.vue
@@ -530,6 +530,40 @@
         </g>
       </g>
     </template>
+    <template v-else-if="id === 'users'">
+      <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+      <circle cx="9" cy="7" r="4"></circle>
+      <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+      <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+    </template>
+    <template v-else-if="id === 'mail-outline'">
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-mail"
+      >
+        <path
+          d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+        ></path>
+        <polyline points="22,6 12,13 2,6"></polyline>
+      </g>
+    </template>
+    <template v-else-if="id === 'clock'">
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-clock"
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <polyline points="12 6 12 12 16 14"></polyline>
+      </g>
+    </template>
   </svg>
 </template>
 
