1ca60ca8e500db9571c9aeb99c0e10843fbd6ae2
Florian Merz <flomerz@gmail.com>
2019-11-20 14:06:06 +0100

    preview infra support

---
+++ terraform/codebuild-preview/provider.tf
@@ -0,0 +1,18 @@
+#---
+# Provider Configuration
+#---
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_version = "~> 0.11"
+
+  backend "s3" {
+    bucket = "eks-terraform-shared-state"
+    key    = "global/codebuild/dino-park-front-end-preview/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
