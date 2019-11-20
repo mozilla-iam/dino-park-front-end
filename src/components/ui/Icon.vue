58e0a18f74cb14f47070920a1921a189957a8a05
Andrew Sunada <asunada@mozilla.com>
2019-11-20 13:57:57 -0800

    feat(IAM-293): adding curator crown to curator members

--- src/components/ui/Icon.vue
+++ src/components/ui/Icon.vue
@@ -253,22 +253,8 @@
       />
     </template>
     <template v-else-if="id === 'x'">
-      <line
-        x1="18"
-        y1="6"
-        x2="6"
-        y2="18"
-        stroke="currentColor"
-        stroke-width="2"
-      />
-      <line
-        x1="6"
-        y1="6"
-        x2="18"
-        y2="18"
-        stroke="currentColor"
-        stroke-width="2"
-      />
+      <line x1="18" y1="6" x2="6" y2="18" stroke="currentColor" stroke-width="2" />
+      <line x1="6" y1="6" x2="18" y2="18" stroke="currentColor" stroke-width="2" />
     </template>
     <template v-else-if="id === 'plus'">
       <path
@@ -335,27 +321,19 @@
         <path
           d="M14.5 10c-.83 0-1.5-.67-1.5-1.5v-5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5z"
         />
-        <path
-          d="M20.5 10H19V8.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"
-        />
+        <path d="M20.5 10H19V8.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z" />
         <path
           d="M9.5 14c.83 0 1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5S8 21.33 8 20.5v-5c0-.83.67-1.5 1.5-1.5z"
         />
-        <path
-          d="M3.5 14H5v1.5c0 .83-.67 1.5-1.5 1.5S2 16.33 2 15.5 2.67 14 3.5 14z"
-        />
+        <path d="M3.5 14H5v1.5c0 .83-.67 1.5-1.5 1.5S2 16.33 2 15.5 2.67 14 3.5 14z" />
         <path
           d="M14 14.5c0-.83.67-1.5 1.5-1.5h5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-5c-.83 0-1.5-.67-1.5-1.5z"
         />
-        <path
-          d="M15.5 19H14v1.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"
-        />
+        <path d="M15.5 19H14v1.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z" />
         <path
           d="M10 9.5C10 8.67 9.33 8 8.5 8h-5C2.67 8 2 8.67 2 9.5S2.67 11 3.5 11h5c.83 0 1.5-.67 1.5-1.5z"
         />
-        <path
-          d="M8.5 5H10V3.5C10 2.67 9.33 2 8.5 2S7 2.67 7 3.5 7.67 5 8.5 5z"
-        />
+        <path d="M8.5 5H10V3.5C10 2.67 9.33 2 8.5 2S7 2.67 7 3.5 7.67 5 8.5 5z" />
       </g>
     </template>
     <template v-else-if="id === 'rotate'">
@@ -531,6 +509,27 @@
         height="24"
       />
     </template>
+    <template v-else-if="id === 'crown-fill'">
+      <defs>
+        <path
+          d="M1,17 L1,7.13504161 C1,6.58275686 1.44771525,6.13504161 2,6.13504161 C2.23393651,6.13504161 2.4604694,6.21705783 2.6401844,6.36682033 L7,10 L11.2928932,5.70710678 C11.6834175,5.31658249 12.3165825,5.31658249 12.7071068,5.70710678 L17,10 L21.3598156,6.36682033 C21.7840925,6.01325625 22.4146572,6.07058032 22.7682213,6.49485721 C22.9179838,6.67457221 23,6.90110511 23,7.13504161 L23,17 C23,18.1045695 22.1045695,19 21,19 L3,19 C1.8954305,19 1,18.1045695 1,17 Z"
+          id="path-1"
+        ></path>
+      </defs>
+      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="icon-/-crown-fill" transform="translate(-1.000000, 0.000000)">
+          <mask id="mask-2" fill="white">
+            <use xlink:href="#path-1"></use>
+          </mask>
+          <use
+            id="Path-2"
+            fill="currentColor"
+            fill-rule="nonzero"
+            xlink:href="#path-1"
+          ></use>
+        </g>
+      </g>
+    </template>
   </svg>
 </template>
 
@@ -546,7 +545,12 @@ export default {
     id: String,
   },
   mounted() {
-    if (this.id === 'self-avatar') {
+    generateIdenticon(this.$store.state.user.primaryUsername.value, 40).then(a => {
+      this.generatedAvatarUrl = a;
+    });
+  },
+  computed: {
+    computedAvatarUrl() {
       if (this.$store.state.user.picture.value) {
         this.avatarUrl = avatarUrl(
           this.$store.state.user.picture.value,
