652851d4a9472e68cff2441d4f02aa7f7f248072
Andrew Sunada <asunada@mozilla.com>
2019-11-20 15:14:25 -0800

    feat(IAM-293): adding group details component

---
+++ src/components/access_group/AccessGroupDetails.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="membership-management-container">
+    <p class="primary-data-row">{{ membersCountText }}</p>
+    <a class="primary-data-row" v-if="model.group.terms" :href="tosUrl">Group Terms</a>
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/ui/Icon.vue';
+import LinksMixin from '@/components/_mixins/LinksMixin.vue';
+import UserPicture from '@/components/ui/UserPicture.vue';
+
+export default {
+  name: 'AccessGroupDetails',
+  components: { Icon, UserPicture },
+  mixins: [LinksMixin],
+  props: {
+    title: String,
+    editable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    membersCountText() {
+      let fullText = '';
+      if (this.model.member_count === 1) {
+        fullText += '1 member';
+      } else {
+        fullText += `${this.model.member_count} members`;
+      }
+      return fullText;
+    },
+  },
+  data() {
+    return {
+      model: this.$store.state.accessGroup,
+    };
+  },
+};
+</script>
+
+<style></style>
