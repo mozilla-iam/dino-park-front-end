447c39ba8057c8f19b9e4fe03ccf3f7066d28eca
Florian Merz <flomerz@gmail.com>
2020-01-22 16:41:53 +0100

    Revert "add matrix as external account"

--- src/components/_mixins/AccountsMixin.vue
+++ src/components/_mixins/AccountsMixin.vue
@@ -1,5 +1,5 @@
 <script>
-const ENABLED_ACCOUNTS = ['SLACK', 'ZOOM', 'DISCOURSE', 'IRC', 'MOZMATRIX'];
+const ENABLED_ACCOUNTS = ['SLACK', 'ZOOM', 'DISCOURSE', 'IRC'];
 const EXTERNAL_ACCOUNTS = {
   AIM: { moz: false, text: 'AIM', icon: 'aim' },
   BITBUCKET: { moz: false, text: 'Bitbucket', icon: 'bitbucket' },
@@ -16,11 +16,6 @@ const EXTERNAL_ACCOUNTS = {
     text: 'Mozilla Discourse',
     icon: 'discourse',
     uri: 'https://discourse.mozilla.org/u/@@@',
-  },
-  MOZMATRIX: {
-    moz: true,
-    text: 'Mozilla Matrix',
-    icon: 'matrix',
   },
   MOZPHAB: { moz: true, text: 'Mozilla Phabricator', icon: 'mozphab' },
   MOZILLAPONTOON: {
