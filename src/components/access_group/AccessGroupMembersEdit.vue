2020917c5f884660afc88fbff243c3c04c6f9167
Andrew Sunada <asunada@mozilla.com>
2020-04-09 12:23:06 -0700

    fix(IAM-604): fixing table styles

--- src/components/access_group/AccessGroupMembersEdit.vue
+++ src/components/access_group/AccessGroupMembersEdit.vue
@@ -7,9 +7,9 @@
       <template v-slot:content>
         <p class="edit-members-meta">
           {{ fluent('access-group_members', 'edit-members-meta') }}
-          <output class="edit-members-meta__focus">
-            {{ expirationMetaText }}
-          </output>
+          <output class="edit-members-meta__focus">{{
+            expirationMetaText
+          }}</output>
         </p>
         <div class="members-list-container">
           <header class="members-list-container__controls">
@@ -62,9 +62,10 @@
                         expiration: selectedRowExpiration,
                       })
                     "
+                    >{{
+                      fluent('access-group_members', 'renew-action')
+                    }}</Button
                   >
-                    {{ fluent('access-group_members', 'renew-action') }}
-                  </Button>
                 </div>
               </div>
             </div>
@@ -171,9 +172,9 @@
       <template v-slot:content>
         <div class="members-expiration-container">
           <div class="content-area__row expiration-container">
-            <label class="content-area__label expiration-container__label">{{
-              fluent('access-group_expiration', 'expiration__description')
-            }}</label>
+            <label class="content-area__label expiration-container__label">
+              {{ fluent('access-group_expiration', 'expiration__description') }}
+            </label>
             <RadioSelect
               class="expiration-container__value"
               :options="expirationOptions"
@@ -245,7 +246,10 @@ import {
   MEMBER_EXPIRATION_ONE_YEAR,
   MEMBER_EXPIRATION_TWO_YEARS,
 } from '@/view_models/AccessGroupViewModel';
-import { expiryTextFromDate, getExpirationDate } from '@/assets/js/component-utils';
+import {
+  expiryTextFromDate,
+  getExpirationDate,
+} from '@/assets/js/component-utils';
 import AccessGroups from '@/assets/js/access-groups';
 
 const memberRenewalThreshold = 14;
@@ -590,6 +594,18 @@ export default {
 }
 .members-list-container .edit-members__search {
   margin: 0 2em 1em;
+}
+
+.members-list-container .edit-members__search {
+  margin: 0 2em 1em;
+}
+
+.members-list-container .edit-members__search .search-form__input {
+  padding-left: 3.5em;
+}
+
+.members-list-container .edit-members__search .search-form__input::placeholder {
+  text-align: left;
 }
 .members-list-container .sort-select.options {
   align-self: initial;
