3668b63df0be424c7feb4240ddff55341a43ae2a
Florian Merz <me@fiji-flo.de>
2020-04-15 15:59:37 +0200

    update all
    - hack to finall update to preact X
    - TODO: investigate how to properly do this

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['@vue/app'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./node_modules'],
+        alias: {
+          '^preact$': 'preact/compat',
+          '^hack/preact$': 'preact',
+        },
+      },
+    ],
+  ],
 };
