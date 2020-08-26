21a1aeaf3fefe999fa19b6895f3b7c46e62ba9c6
Jens Hausdorf <mail@jens-hausdorf.de>
2020-08-26 12:03:05 +0200

    Fix TOSAcceptanceNotification to appear after you have accepted the TOS

--- src/components/access_group/AccessGroupTOSAcceptanceNotification.vue
+++ src/components/access_group/AccessGroupTOSAcceptanceNotification.vue
@@ -1,8 +1,5 @@
 <template>
-  <article
-    class="notification-container tos-acceptance-notification-container"
-    v-if="showTOSAcceptanceNotification"
-  >
+  <article class="notification-container tos-acceptance-notification-container">
     <span class="acceptance-notification__icon">
       <Icon id="info" :width="32" :height="32" />
     </span>
@@ -13,63 +10,25 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 import Icon from '@/components/ui/Icon.vue';
-import { ACCESS_GROUP_TOS_PAGE } from '@/router';
-import { Api } from '@/assets/js/access-groups-api.js';
-import { GroupInvitationViewModel } from '@/view_models/AccessGroupViewModel';
 
-const accessGroupApi = new Api();
 export default {
   name: 'AccessGroupTOSAcceptanceNotification',
   props: {},
   components: { Icon },
-  methods: {
-    async getInvitationByName(groupName) {
-      const invitations = (
-        await accessGroupApi.execute({
-          path: 'selfInvitations/get',
-        })
-      ).map((invite) => new GroupInvitationViewModel(invite));
-
-      for (const invite of invitations) {
-        if (invite.groupName === groupName) {
-          return invite;
-        }
-      }
-      return null;
-    },
-  },
-  computed: {
-    showTOSAcceptanceNotification() {
-      if (
-        this.$route.name !== ACCESS_GROUP_TOS_PAGE ||
-        !this.$route.query.accept
-      ) {
-        return false;
-      }
-
-      const invitationByName = this.getInvitationByName(
-        this.$route.params.groupname,
-      );
-      if (!invitationByName) {
-        return false;
-      }
-      return invitationByName.requiresTos;
-    },
-  },
 };
 </script>
 
 <style>
 .tos-acceptance-notification-container {
+  align-items: center;
   background: #f2fcfd;
   border: 1px solid var(--blue-60);
   border-radius: var(--formElementRadius);
-  padding: 0.5em 1em;
   display: flex;
   flex-direction: row;
-  align-items: center;
+  padding: 0.5em 1em;
+  margin-bottom: 2em;
 }
 
 .tos-acceptance-notification-container .acceptance-notification__icon {
