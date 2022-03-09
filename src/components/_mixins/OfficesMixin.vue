0302191e3a4440b8896806df5cf920d16449c378
Claas Augner <caugner@mozilla.com>
2022-03-09 20:32:12 +0100

    chore(OfficesMixin): update offices + floor plan link

--- src/components/_mixins/OfficesMixin.vue
+++ src/components/_mixins/OfficesMixin.vue
@@ -1,24 +1,18 @@
 <script>
-const OFFICES = {
-  BER: 'Berlin+Office+Floor+plan+-+BER',
-  LON: 'London+Office+Floor+plan+-+LON',
-  MTV: 'Mountain+View+Office+Floor+plans+-+MTV',
-  PAR: 'Paris+Office+Floor+plans+-+PAR',
-  PDX: 'Portland+Office+Floor+plan+-+PDX',
-  SF: 'San+Francisco+Office+Floor+plans+-+SFO',
-  TPE: 'Taipei+Office+Floor+plan+-+TPE',
-  TOR: 'Toronto+Office+Floor+plan-+TOR',
-  YVR: 'Vancouver+Office+Floor+plan+-+YVR',
-};
+const OFFICES = [
+  'BER',
+  'PAR',
+  'PDX',
+  'SF',
+  'TOR',
+];
 
 export default {
   methods: {
     deskNumberToOfficePlanLink(deskNumber) {
       if (typeof deskNumber === 'string') {
-        const name =
-          OFFICES[deskNumber.slice(0, 3)] || OFFICES[deskNumber.slice(0, 2)];
-        if (name) {
-          return `https://mana.mozilla.org/wiki/display/WPR/${name}`;
+        if (OFFICES.some(office => deskNumber.startsWith(office))) {
+          return 'https://mana.mozilla.org/wiki/display/WPR/MozSpaces+Floor+Plans';
         }
       }
       return null;
