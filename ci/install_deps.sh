ae88bf46e065cb24f4b96d1ab95706059434aa27
atoll <atoll@mozilla.com>
2022-11-08 16:08:58 -0800

    Silence curl fetch progress output

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -1,6 +1,6 @@
 HELM_INSTALL_DIR=/bin
 export DESIRED_VERSION="v3.5.4"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
-curl -L -o myke https://github.com/fiji-flo/myke/releases/download/0.9.11/myke-0.9.11-x86_64-unknown-linux-musl
+curl -s -L -o myke https://github.com/fiji-flo/myke/releases/download/0.9.11/myke-0.9.11-x86_64-unknown-linux-musl
 chmod +x ./myke
 mv ./myke /bin/myke
